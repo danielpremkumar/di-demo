3629b4d238f77ef9b6a835a7c31f032977b704be
nerdylegend <31123377+nerdylegend@users.noreply.github.com>
2018-12-06 06:13:05 +0530

    Commit

--- src/main/java/com/nerdylegend/didemo/controllers/MyController.java
+++ src/main/java/com/nerdylegend/didemo/controllers/MyController.java
@@ -1,4 +1,12 @@
 package com.nerdylegend.didemo.controllers;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class MyController {
+
+    public String sayHello() {
+        System.out.println("Hello");
+        return "Hi";
+    }
 }
